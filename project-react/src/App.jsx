ffde5b901117706b89332ff1146f87b7996c2a9b
yago <yago.zhvaniya@gmail.com>
2022-04-06 17:39:41 +0300

    HomeWork-1

---
+++ project-react/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Massage } from './components/Message/Message';
+import './App.css';
+
+export const App = () => {
+  return <>
+  <div >
+    <h1 className='wrapper'>ReactJS</h1>
+  <Massage />
+  </div>
+  </>
+}
+
---
+++ project-react/src/components/Message/MassageText.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+
+
+export const MassageText = (props) => {
+    return <>
+        <h1 className="styleMassage">Hello React... 
+        <br />
+        {props.name}</h1>
+        </>
+}
